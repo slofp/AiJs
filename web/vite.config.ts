e05a119d6ab9f747aa65ac3a84bb1c99c4bce7ff
Fairy-Phy <phy.public@gmail.com>
2023-12-13 19:44:38 +0900

    fix web action and vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: process.env.IS_GITHUB_ACTION ? "" : "./",
+    base: process.env.IS_GITHUB_ACTION ? "/AiJs/" : "/",
     plugins: [svelte()],
     optimizeDeps: {
         exclude: ["@slofp/aijs"],
