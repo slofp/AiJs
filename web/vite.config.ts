00119ebfb93e5296a75c5ed5e99a60e5e1e5a7a6
Fairy-Phy <phy.public@gmail.com>
2023-12-13 19:24:25 +0900

    add deploy web action

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: process.env.IS_GITHUB_ACTION ? "" : "./",
     plugins: [svelte()],
     optimizeDeps: {
         exclude: ["@slofp/aijs"],
