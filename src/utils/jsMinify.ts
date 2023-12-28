c75c1fbabc95a515abedb3472dff9c554b36ef47
Fairy-Phy <phy.public@gmail.com>
2023-12-29 04:32:56 +0900

    property minify

--- src/utils/jsMinify.ts
+++ src/utils/jsMinify.ts
@@ -1,5 +1,6 @@
 import { MinifyOptions, minify } from 'terser';
 import { base53 } from './base53';
+import { excludeRegExp } from './stdProp';
 
 const options: MinifyOptions = {
 	mangle: {
@@ -8,10 +9,37 @@ const options: MinifyOptions = {
 		toplevel: true,
 		module: true,
 		nth_identifier: base53,
+		reserved: [],
+		properties: {
+			//debug: 'Mangled' as unknown as boolean,
+			keep_quoted: false,
+			nth_identifier: base53,
+			regex: excludeRegExp,
+			reserved: [],
+		}
 	},
-	compress: false,
+	compress: {
+		defaults: false,
+		dead_code: true,
+		directives: true,
+		drop_console: true,
+		drop_debugger: true,
+		ecma: 2020,
+		evaluate: true,
+		properties: true,
+		passes: 2,
+		module: true,
+		//loops: true,
+		keep_classnames: false,
+		keep_fargs: false,
+		keep_fnames: false,
+		keep_infinity: false,
+		switches: true,
+		unused: true,
+	},
 	keep_classnames: false,
 	keep_fnames: false,
+	module: true
 };
 
 export async function minifyIdentifier(source: string) {
